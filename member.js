93813c7469531b26217a36028d8fa4d595971e83
Sajal Mukherjee <39090442+sajal-mukherjee-git@users.noreply.github.com>
2025-02-25 05:48:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+  return [
+    {
+      name: 'C#',
+      level: '90%'
+    },
+    {
+      name: 'Javascript',
+      level: '80%'
+    },
+    {
+      name: 'Python',
+      level: '70%'
+    },
+    {
+      name: 'Java',
+      level: '60%'
+    },
+    {
+      name: 'SQL',
+      level: '50%'
+    }
+  ];
+}
